1be76d827d6f936f0a777b2c182dca18695230e3
Rahul Verma <rahul.uoa@gmail.com>
2023-04-23 15:52:01 -0700

    Complete History Feature with History Adapter, History Database, Application, Dao and Entity

--- app/src/main/java/com/example/a7minuteworkout/FinishActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/FinishActivity.kt
@@ -2,7 +2,12 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minuteworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -23,6 +28,29 @@ class FinishActivity : AppCompatActivity() {
 
         binding?.btnFinish?.setOnClickListener {
             finish()
+        }
+
+        val historyDao = (application as WorkoutApp).db.historyDao()
+        addDateToDatabase(historyDao)
+
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao) {
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        Log.e("Date: ", "" +dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date : ", "" + date)
+
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date))
+            Log.e(
+                "Date: ",
+                "Added..."
+            )
         }
 
     }
--- app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
@@ -2,7 +2,11 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a7minuteworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -13,14 +17,52 @@ class HistoryActivity : AppCompatActivity() {
         binding = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        setSupportActionBar(binding?.toolbarActivityHistory)
+        setSupportActionBar(binding?.toolbarHistoryActivity)
         if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
             supportActionBar?.title = "HISTORY"
         }
 
-        binding?.toolbarActivityHistory?.setNavigationOnClickListener() {
+        binding?.toolbarHistoryActivity?.setNavigationOnClickListener() {
             onBackPressed()
         }
+
+        val historyDao = (application as WorkoutApp).db.historyDao()
+        getAllCompletedDates(historyDao)
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao) {
+        lifecycleScope.launch {
+            historyDao.fetchAllDates().collect{ allCompletedDatesList ->
+                if (allCompletedDatesList.isNotEmpty()) {
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+
+                    val dates = ArrayList<String>()
+                    for (date in allCompletedDatesList) {
+                        dates.add(date.date)
+                    }
+
+                    val historyAdapter = HistoryAdapter(dates)
+                    binding?.rvHistory?.adapter = historyAdapter
+
+
+                } else {
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+
+                }
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
